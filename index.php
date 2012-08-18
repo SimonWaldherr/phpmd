94cc701ef902c0d3acb7dce31e9719d25a1c2738
Simon Waldherr <git@simon.waldherr.eu>
2012-08-19 00:45:25 +0200

    no message

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  
+  <link href="style.css" rel="stylesheet" type="text/css">
+  
+</head>
+<body>
+  
 <?php
 
 include("./phpmd.php");
 
 $string = $_POST['textarea'];
 
-echo markdown($string);
+echo '<div id="html">'.markdown($string).'</div>';
 
-echo '<hr><form action="" method="post"><p><textarea id="textarea" name="textarea">'.$string.'</textarea><input type="submit" value="Next"></p></form>';
+echo '<hr><div id="md"><form action="" method="post"><p><textarea id="textarea" name="textarea">'.$string.'</textarea><input type="submit" value="Next"></p></form></div>';
 
 ?>
+  
+</body>
+</html>
